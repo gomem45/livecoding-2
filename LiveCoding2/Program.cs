cc29bfc204aafc2bcf92016b113ac23c98fb98a6
Miguel Gomez <mgomez.developer@gmail.com>
2019-11-19 19:06:43 -0500

    Add comments for live coding 2

---
+++ LiveCoding2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LiveCoding2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] test = {
+                "aaa",
+                "a",
+                "bdsagbgagggaaatttyyyyau",
+                "abcdefghijklmnopqrstuvwxyz",
+                ""
+            };
+
+            string maxWord = "";
+            int wordCount = 0;
+
+            foreach (string chr in test)
+            {
+                if (chr.Length > wordCount)
+                {
+                    wordCount = chr.Length;
+                    maxWord = chr;
+                }
+            }
+
+            Console.WriteLine("Longest string of chars: {0} \nLenght of longest chars: {1}", maxWord, wordCount);
+            Console.ReadLine();
+            
+        }
+    }
+}
